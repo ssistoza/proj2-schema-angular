a965618757cdb2be96894319c22238596ef3218d
Shane Avery Sistoza <ss2010@hawaii.edu>
2018-01-23 21:57:54 -0500

    adding, but production server api must be added.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,47 @@
+// Must be change to our production server.
+const context = 'http://localhost:8090/scrumhub/api/dev';
+
 export const environment = {
-  production: true
+  production: true,
+  context: context,
+
+  user: {
+  	create: () => `${context}/user/create`,
+  	get: (userid: number) => `${context}/user/${userid}`,
+  	all: () => `${context}/user/all`,
+  	update: () => `${context}/user/update`,
+  	delete: () => `${context}/user/delete`,
+  	login: () => `${context}/user/login`
+  },
+
+  board: {
+  	create: () => `${context}/board/create`,
+  	update: () => `${context}/board/update`
+  },
+
+  boardmember: {
+  	create: () => `${context}/boardmember/create`,
+  	update: () => `${context}/boardmember/update`,
+  	delete: () => `${context}/boardmember/delete`
+  },
+
+  roles: {
+  	get: (roleid: number) => `${context}/roles/${roleid}`
+  },
+
+  swimlanestatus: {
+  	get: (statusid: number) => `${context}/swimlanestatus/${statusid}`,
+  	update: () => `${context}/swimlanestatus/update`
+  },
+
+  story: {
+  	get: (storyid: number) => `${context}/story/${storyid}`,
+  	create: () => `${context}/story/create`
+  },
+
+  task: {
+  	get: (taskid: number) => `${context}/task/${taskid}`,
+  	create: () => `${context}/task/create`,
+  	update: () => `${context}/task/update`
+  }
 };
