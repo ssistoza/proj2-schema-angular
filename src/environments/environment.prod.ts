a6dc9f37d5e22751a12e59c2852d02e8e9693822
Shane Avery Sistoza <ss2010@hawaii.edu>
2018-01-26 23:05:35 -0500

    changes to api. please use the context in your services from now on.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,47 +1,47 @@
 // Must be change to our production server.
-const context = 'http://localhost:8090/scrumhub/api/dev';
+const context = 'http://18.219.30.69:8090/scrumhub/api';
 
 export const environment = {
   production: true,
   context: context,
 
   user: {
-  	create: () => `${context}/user/create`,
-  	get: (userid: number) => `${context}/user/${userid}`,
-  	all: () => `${context}/user/all`,
-  	update: () => `${context}/user/update`,
-  	delete: () => `${context}/user/delete`,
-  	login: () => `${context}/user/login`
+    create: () => `${context}/user/create`,
+    get: (userid: number) => `${context}/user/${userid}`,
+    all: () => `${context}/user/all`,
+    update: () => `${context}/user/update`,
+    delete: () => `${context}/user/delete`,
+    login: () => `${context}/user/login`
   },
 
   board: {
-  	create: () => `${context}/board/create`,
-  	update: () => `${context}/board/update`
+    create: () => `${context}/board/create`,
+    update: () => `${context}/board/update`
   },
 
   boardmember: {
-  	create: () => `${context}/boardmember/create`,
-  	update: () => `${context}/boardmember/update`,
-  	delete: () => `${context}/boardmember/delete`
+    create: () => `${context}/boardmember/create`,
+    update: () => `${context}/boardmember/update`,
+    delete: () => `${context}/boardmember/delete`
   },
 
   roles: {
-  	get: (roleid: number) => `${context}/roles/${roleid}`
+    get: (roleid: number) => `${context}/roles/${roleid}`
   },
 
   swimlanestatus: {
-  	get: (statusid: number) => `${context}/swimlanestatus/${statusid}`,
-  	update: () => `${context}/swimlanestatus/update`
+    get: (statusid: number) => `${context}/swimlanestatus/${statusid}`,
+    update: () => `${context}/swimlanestatus/update`
   },
 
   story: {
-  	get: (storyid: number) => `${context}/story/${storyid}`,
-  	create: () => `${context}/story/create`
+    get: (storyid: number) => `${context}/story/${storyid}`,
+    create: () => `${context}/story/create`
   },
 
   task: {
-  	get: (taskid: number) => `${context}/task/${taskid}`,
-  	create: () => `${context}/task/create`,
-  	update: () => `${context}/task/update`
+    get: (taskid: number) => `${context}/task/${taskid}`,
+    create: () => `${context}/task/create`,
+    update: () => `${context}/task/update`
   }
 };
