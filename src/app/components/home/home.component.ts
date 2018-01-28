5fb02e0252a085e01c23a803c1d0c89d670888ce
Shane Avery Sistoza <ss2010@hawaii.edu>
2018-01-27 20:46:30 -0500

    changes to home board list view.

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -7,6 +7,7 @@ import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap/modal/modal-ref';
 import {NgbModal, ModalDismissReasons} from '@ng-bootstrap/ng-bootstrap';
 import {BoardComponent} from '../board/board.component';
 import { NavbarService } from '../../services/navbar/navbar.service';
+import { SessionService } from '../../services/session.service';
 
 @Component({
   selector: 'app-home',
@@ -21,21 +22,23 @@ export class HomeComponent implements OnInit {
   constructor(private accountService: ScrumUserAccountService,
               private boardService: BoardService,
               private modalService: NgbModal,
-              private navService: NavbarService
+              private navService: NavbarService,
+              private sessionService: SessionService
   ) { }
 
-  open(board) {
-    console.log(board);
-const modalRef = this.modalService.open(BoardComponent);
-modalRef.componentInstance.board = board;
-}
+  open(board, e: Event) {
+    const modalRef = this.modalService.open(BoardComponent);
+    e.stopPropagation();
+    modalRef.componentInstance.board = board;
+  }
+  
   openToAdd(): void {
     const modalRef = this.modalService.open(BoardComponent);
 
   }
 
   getUserInfo() {
-    this.accountService.getScrumUserAccount(1).subscribe(
+    this.accountService.getScrumUserAccount(this.sessionService.getScrumUserId()).subscribe(
       service => this.scrumUser = service
     );
 
